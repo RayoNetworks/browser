9902dce3438890b42eaabf76660a04a3d5f8a06f
Daniel Iheonu <64360587+danieldutcum@users.noreply.github.com>
2021-10-04 13:13:15 +0100

    implemented proxy server

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import sys
-from PyQt5.QtCore import *
+from PyQt5.QtCore import
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 
+import proxy_server
+
+import threading
 
 class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
+    def __init__(self, callback):
+        super(MainWindow, self).__init__()  
         self.browser = QWebEngineView()
         self.browser.setUrl(QUrl('http://google.com'))
         self.setCentralWidget(self.browser)
@@ -37,12 +40,21 @@ class MainWindow(QMainWindow):
         navbar.addWidget(self.url_bar)
 
         self.browser.urlChanged.connect(self.update_url)
+        
+        # callback()
+        server = threading.Thread(target=proxy_server.server)
+        server.start()
 
     def navigate_home(self):
         self.browser.setUrl(QUrl('https://echoboo.com'))
 
     def navigate_to_url(self):
-        url = self.url_bar.text()
+
+        if 'https://' in self.url_bar.text():
+            url = self.url_bar.text()
+        else:
+            url = f'https://{self.url_bar.text()}'
+
         self.browser.setUrl(QUrl(url))
 
     def update_url(self, q):
@@ -51,5 +63,6 @@ class MainWindow(QMainWindow):
 
 app = QApplication(sys.argv)
 QApplication.setApplicationName('Uleval Browser')
-window = MainWindow()
+window = MainWindow(proxy_server)
 app.exec_()
+
